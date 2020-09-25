30fe1095b337858ca093781e9c23133427925cef
capodabo <71343016+capodabo@users.noreply.github.com>
2020-09-25 21:19:26 +0100

    shite

--- src/main/java/Proba.java
+++ src/main/java/Proba.java
@@ -1,13 +1,33 @@
+import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class Proba {
+class Proba {
 
+    public static void main(String[] args) throws IOException {
+        int serverPort = 8088;
+        HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
+        server.createContext("/world-time", (exchange -> {
+            String respText = null;
+            try {
+                respText = httpRead();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            exchange.sendResponseHeaders(200, respText.getBytes().length);
+            OutputStream output = exchange.getResponseBody();
+            output.write(respText.getBytes());
+            output.flush();
+            exchange.close();
+        }));
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
     public static String httpRead() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -16,26 +36,5 @@ public class Proba {
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         return response.body();
-        }
-
-        public static void soseta() throws IOException {
-            ServerSocket server = new ServerSocket(8088);
-            while (true) {
-                try {
-                    Socket socket = server.accept();
-                    String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + httpRead();
-                    socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
-                    socket.close();
-                } catch (IOException | InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
     }
-
-        public static void main (String[]args) throws IOException {
-            soseta();
-        }
 }
-
-
-
